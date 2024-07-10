fc29c1e4eb08b224d8635cc06efd3fbbc10d7815
Eltrone <amineelorche@gmail.com>
2024-07-10 15:28:02 +0000

    file added

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,6 +1,9 @@
 -- Drop the trigger if it already exists
 DROP TRIGGER IF EXISTS after_order_insert;
 
+-- Change the delimiter to avoid issues with semicolons within the trigger body
+DELIMITER $$
+
 -- Create a new trigger
 CREATE TRIGGER after_order_insert
 AFTER INSERT ON orders
@@ -9,4 +12,7 @@ BEGIN
     UPDATE items
     SET quantity = quantity - NEW.number
     WHERE name = NEW.item_name;
-END;
+END $$
+
+-- Reset the delimiter back to semicolon
+DELIMITER ;
