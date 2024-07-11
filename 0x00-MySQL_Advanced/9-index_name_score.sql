9935e6503b00a8ea1c7ed8f46adc3b59d9b187c2
Eltrone <amineelorche@gmail.com>
2024-07-11 19:34:14 +0000

    file added

--- 0x00-MySQL_Advanced/9-index_name_score.sql
+++ 0x00-MySQL_Advanced/9-index_name_score.sql
@@ -2,4 +2,4 @@
 DROP INDEX IF EXISTS idx_name_first_score ON names;
 
 -- Create a new index on the first letter of the name and the score
-CREATE INDEX idx_name_first_score ON names (SUBSTRING(name, 1), score);
+CREATE INDEX idx_name_first_score ON names (name, 1), score);
