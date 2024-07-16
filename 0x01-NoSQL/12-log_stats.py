3ead24d5addb13293061bdb3d5fcc9bf0203f9ac
Eltrone <amineelorche@gmail.com>
2024-07-16 12:29:58 -0700

    file modified

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,48 +1,32 @@
 #!/usr/bin/env python3
-"""
-This script provides statistics about Nginx logs stored in a MongoDB database.
-It connects to the MongoDB, queries the logs collection, and displays various statistics.
-"""
+"""This script gathers and displays statistics from Nginx logs stored in a MongoDB database."""
 
-import pymongo
+from pymongo import MongoClient
 
-def connect_to_mongodb():
+def log_stats():
     """
-    Establishes a connection to the MongoDB database.
-    Returns:
-        Collection object for the nginx logs.
+    Fetches and prints statistics about nginx logs from the MongoDB 'logs' database.
+    Statistics include total log entries and counts of different HTTP methods, including a specific check for GET requests to '/status'.
     """
-    client = pymongo.MongoClient('mongodb://localhost:27017/')
-    db = client.logs
-    return db.nginx
-
-def display_stats(collection):
-    """
-    Displays statistics about Nginx logs stored in the MongoDB collection.
-    Args:
-        collection: The MongoDB collection containing nginx logs.
-    """
-    # Calculating total number of documents
-    total_logs = collection.count_documents({})
-    print(f"{total_logs} logs")
-
-    # Displaying methods count
-    methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    logs_collection = client.logs.nginx
+    total = logs_collection.count_documents({})
+    get = logs_collection.count_documents({"method": "GET"})
+    post = logs_collection.count_documents({"method": "POST"})
+    put = logs_collection.count_documents({"method": "PUT"})
+    patch = logs_collection.count_documents({"method": "PATCH"})
+    delete = logs_collection.count_documents({"method": "DELETE"})
+    path = logs_collection.count_documents(
+            {"method": "GET", "path": "/status"})
+    
+    print(f"{total} logs")
     print("Methods:")
-    for method in methods:
-        count = collection.count_documents({'method': method})
-        print(f"    method {method}: {count}")
-
-    # Counting documents where method=GET and path=/status
-    status_checks = collection.count_documents({'method': 'GET', 'path': '/status'})
-    print(f"{status_checks} status check")
-
-def main():
-    """
-    Main function to execute script logic.
-    """
-    collection = connect_to_mongodb()
-    display_stats(collection)
+    print(f"\tmethod GET: {get}")
+    print(f"\tmethod POST: {post}")
+    print(f"\tmethod PUT: {put}")
+    print(f"\tmethod PATCH: {patch}")
+    print(f"\tmethod DELETE: {delete}")
+    print(f"{path} status check")
 
 if __name__ == "__main__":
-    main()
+    log_stats()
